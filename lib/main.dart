9584974ab80c042e6fecaf85a57050ff326022ef
Catalina <catalina.suarez05@inacapmail.cl>
2021-10-14 00:32:34 -0300

    ¡Bienvenido!

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '¡Bienvenido!',
             ),
             Text(
               '$_counter',
@@ -107,11 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
